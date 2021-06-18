fa635acbdf1a79080d15a7a75929489a4b935ad3
YangHao <yanghao@luojilab.com>
2021-06-18 22:59:03 +0800

    graceful restart http server

--- gin-tutorial/main.go
+++ gin-tutorial/main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"gin-tutorial/pkg/settings"
 	"gin-tutorial/routers"
+	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"time"
 )
 
 func main() {
@@ -14,17 +19,62 @@ func main() {
 	//		"message": "pong",
 	//	})
 	//})
-	router := routers.InitRouter()
 
-	s := http.Server{
+	//router := routers.InitRouter()
+	//
+	//s := http.Server{
+	//	Addr:           fmt.Sprintf(":%d", settings.HTTPPort),
+	//	Handler:        router,
+	//	ReadTimeout:    settings.ReadTimeout,
+	//	WriteTimeout:   settings.WriteTimeout,
+	//	MaxHeaderBytes: 1 << 20,
+	//}
+	//err := s.ListenAndServe()
+	//if err != nil {
+	//	return
+	//}
+
+	// Zero downtime restarts for golang HTTP servers.
+	//endless.DefaultReadTimeOut = settings.ReadTimeout
+	//endless.DefaultWriteTimeOut = settings.WriteTimeout
+	//endless.DefaultMaxHeaderBytes = 1 << 20
+	//endPoint := fmt.Sprintf(":%d", settings.HTTPPort)
+	//
+	//server := endless.NewServer(endPoint, routers.InitRouter())
+	//server.BeforeBegin = func(add string) {
+	//	log.Printf("Actual pid is %d", syscall.Getpid())
+	//}
+	//
+	//err := server.ListenAndServe()
+	//if err != nil {
+	//	log.Printf("Server err: %v", err)
+	//}
+
+	// http.Shutdown
+	router := routers.InitRouter()
+	s := &http.Server{
 		Addr:           fmt.Sprintf(":%d", settings.HTTPPort),
 		Handler:        router,
 		ReadTimeout:    settings.ReadTimeout,
 		WriteTimeout:   settings.WriteTimeout,
 		MaxHeaderBytes: 1 << 20,
 	}
-	err := s.ListenAndServe()
-	if err != nil {
-		return
+
+	go func() {
+		if err := s.ListenAndServe(); err != nil {
+			log.Printf("Listen: %s\n", err)
+		}
+	}()
+
+	quit := make(chan os.Signal)
+	signal.Notify(quit, os.Interrupt)
+	<-quit
+
+	log.Println("Shutdown server ....")
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := s.Shutdown(ctx); err != nil {
+		log.Fatal("Server shutdown: ", err)
 	}
+	log.Println("Server exiting.")
 }
